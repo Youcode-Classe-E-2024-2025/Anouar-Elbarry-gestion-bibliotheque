2d715cc0d3e9616c50f92b1c2ba2f7b2d2d946ad
anouar el barry <elbarryanwar37@gmail.com>
2025-02-17 16:49:54 +0100

    add router for profile and details/change profile style

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/books', function () {
+    return view('books/list');
+});
+Route::get('/books/details', function () {
+    return view('books/details');
+});
+Route::get('/auth', function () {
+    return view('auth');
+});
+Route::get('/profile', function () {
+    return view('profile');
 });
