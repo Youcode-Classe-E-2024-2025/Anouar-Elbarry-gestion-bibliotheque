ca91a47f513c80ba5967ed38fd5c1d33d08db3db
anouar el barry <elbarryanwar37@gmail.com>
2025-02-17 15:21:19 +0100

    add paths on navigation

--- laravel/resources/views/layouts/header.blade.php
+++ laravel/resources/views/layouts/header.blade.php
@@ -3,8 +3,8 @@
         <div class="flex justify-between items-center py-4">
             <div class="text-xl font-semibold text-gray-800">Library</div>
             <div class="space-x-4">
-                <a href="#" class="text-gray-600 hover:text-gray-800">Home</a>
-                <a href="#" class="text-gray-600 hover:text-gray-800">Books</a>
+                <a href="/" class="text-gray-600 hover:text-gray-800">Home</a>
+                <a href="/books" class="text-gray-600 hover:text-gray-800">Books</a>
                 <a href="#" class="text-gray-600 hover:text-gray-800">Categories</a>
             </div>
         </div>
