601d115f0625013a6c2dcba8227beb0251414ff4
anouar el barry <elbarryanwar37@gmail.com>
2025-02-21 00:03:10 +0100

    Update BooksSeeder with more detailed book information and realistic cover images

--- laravel/database/seeders/BooksSeeder.php
+++ laravel/database/seeders/BooksSeeder.php
@@ -17,7 +17,11 @@ class BooksSeeder extends Seeder
             [
                 'title' => 'The Great Gatsby',
                 'desciption' => 'A novel about the decadence of the Jazz Age',
-                'cover_img' => 'https://example.com/gatsby.jpg',
+                'author' => 'ALberto',
+                'price' => '67',
+                'publication_year' => '1904',
+                'available_copies' => '59',
+                'cover_img' => 'https://marketplace.canva.com/EAFfSnGl7II/2/0/1003w/canva-elegant-dark-woods-fantasy-photo-book-cover-vAt8PH1CmqQ.jpg',
                 'is_active' => 1,
                 'created_at' => now(),
                 'updated_at' => now()
@@ -25,7 +29,11 @@ class BooksSeeder extends Seeder
             [
                 'title' => '1984',
                 'desciption' => 'A dystopian social science fiction novel',
-                'cover_img' => 'https://example.com/1984.jpg',
+                'author' => 'Anouar',
+                'price' => '58',
+                'publication_year' => '2005',
+                'available_copies' => '18',
+                'cover_img' => 'https://images-platform.99static.com//-tUjEZmbTnIMpiyAMy6CiS85KYY=/137x101:962x926/fit-in/500x500/projects-files/101/10168/1016819/4d520fb3-bab8-4d4a-a209-17731bb9da86.jpg',
                 'is_active' => 1,
                 'created_at' => now(),
                 'updated_at' => now()
@@ -33,7 +41,11 @@ class BooksSeeder extends Seeder
             [
                 'title' => 'To Kill a Mockingbird',
                 'desciption' => 'A novel about racial injustice in the American South',
-                'cover_img' => 'https://example.com/mockingbird.jpg',
+                'author' => 'ronaldinho',
+                'price' => '120',
+                'publication_year' => '2016',
+                'available_copies' => '35',
+                'cover_img' => 'https://bukovero.com/wp-content/uploads/2016/07/Harry_Potter_and_the_Cursed_Child_Special_Rehearsal_Edition_Book_Cover.jpg',
                 'is_active' => 1,
                 'created_at' => now(),
                 'updated_at' => now()
