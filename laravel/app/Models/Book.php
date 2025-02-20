9d746bb38c54ea454b2c6e15cf67f03e5112e6d5
anouar el barry <elbarryanwar37@gmail.com>
2025-02-20 17:21:31 +0100

    create book model

---
+++ laravel/app/Models/Book.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $table = 'books';
+    
+    protected $fillable = [
+        'title', 
+        'desciption', 
+        'cover_img', 
+        'is_active'    
+    ];
+
+    
+}
